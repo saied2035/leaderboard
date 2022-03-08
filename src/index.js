99b2aa8327a94a1c500b5e5b8d5aece4efca14fd
unknown <saied2421998@gmail.com>
2022-03-08 16:27:20 +0200

    fix reload local storage

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import './style.css';
 import 'tachyons';
-import scores from './modules/scores/scores.js';
 import addScore from './modules/events/events.js';
 import { showScores } from './modules/functions/functions.js';
 
+const scores = [];
 document.querySelector('#score-form').addEventListener('submit', (event) => {
   event.preventDefault();
   addScore(event, scores);
--- src/modules/scores/scores.js
+++
@@ -1,3 +0,0 @@
-const scores = [];
-
-export default scores;
