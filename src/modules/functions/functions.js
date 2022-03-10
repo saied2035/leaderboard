d75d08c83f5b1dca91feb84c9e05ed9d90060441
unknown <saied2421998@gmail.com>
2022-03-10 17:03:46 +0200

    final touches

--- src/modules/functions/functions.js
+++ src/modules/functions/functions.js
@@ -1,21 +1,31 @@
-const showScores = async (game) => {
-  const ul = document.querySelector('#scores-container');
-  ul.innerHTML = '';
+import place1 from '../../images/place1.png';
+import place2 from '../../images/place2.png';
+import place3 from '../../images/place3.png';
 
+const showScores = async (game) => {
+  const postionsArr = [place1, place2, place3];
+  const ul = document.querySelector('#scores-container');
   const scores = await game.loadGameScores();
   if (!scores.length) return;
-
   scores.sort((a, b) => b.score - a.score);
 
-  scores.forEach((item) => {
+  scores.forEach((item, i) => {
     const li = document.createElement('li');
     li.className = 'list-item grow';
-
+    if (i < 3) {
+      const position = document.createElement('img');
+      position.src = postionsArr[i];
+      position.alt = `Medal${i + 1}`;
+      position.width = '25';
+      position.className = 'special-record';
+      position.loading = 'lzay';
+      li.prepend(position);
+    }
     const p = document.createElement('p');
     p.className = 'score';
     p.innerText = ` ${item.user}: ${item.score}`;
 
-    li.appendChild(p);
+    li.prepend(p);
     ul.appendChild(li);
   });
 };
--- src/modules/game/game.js
+++ src/modules/game/game.js
@@ -33,8 +33,14 @@ class Game {
   async loadGameScores() {
     const loadIcon = document.querySelector('#loading');
     loadIcon.classList.remove('dn');
+    const ul = document.querySelector('#scores-container');
+    ul.innerHTML = '';
     const response = await fetch(`${this.url}/${this.gameId}/scores`)
-      .then((res) => res.json());
+      .then((res) => {
+        ul.innerHTML = '';
+        return res.json();
+      });
+
     loadIcon.classList.add('dn');
     return response.result;
   }
